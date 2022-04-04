0908cd1264ddec6618006ae2132a7de9a1490f57
LuisL <luislaviano@gmail.com>
2022-04-04 03:11:45 -0300

    owl-carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                premier_matches: 1
+            
+            },
+            600:{
+                premier_matches:1
+                
+            },
+            1000:{
+                premier_matches: 4
+
+            }
+        }
+    })
   });

c23574428ae28fe33ed13a86310fc35a13e6994f
LuisL <luislaviano@gmail.com>
2022-04-10 16:54:06 -0300

    header sass applied success

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                premier_matches: 1
-            
-            },
-            600:{
-                premier_matches:1
-                
-            },
-            1000:{
-                premier_matches: 1
-
-            }
-        }
-    })
+    $(".owl-carousel").owlCarousel();
   });

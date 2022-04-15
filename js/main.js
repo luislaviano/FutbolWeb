2bc778aa2450c911c7b9f6f38f9b9b42ce87a858
LuisL <luislaviano@gmail.com>
2022-04-15 18:57:57 -0300

    no funcionando

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-$(document).ready(function() {
- 
-    $(".owl-carousel").owlCarousel({
-  
-      responsiveClass:true,
-      responsive:{
-          0:{
-              items:1,
-              nav:true,
-              center: true
-          },
-          600:{
-              items:1,
-              nav:false,
-              center: true
-          },
-          1000:{
-              items:4,
-              nav:true,
-              loop:false
-          }
-      }
-   
-    });
-})
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });

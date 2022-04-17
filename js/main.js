90c23da70e22200d801fdadc5e1f120789fddf10
LuisL <luislaviano@gmail.com>
2022-04-17 06:34:12 -0300

    sass con qatar owl roto

--- js/main.js
+++ js/main.js
@@ -22,4 +22,17 @@ $(document).ready(function() {
       }
    
     });
+
+    $(".owl-fecha-qatar").owlCarousel({
+  
+        responsiveClass:true,
+        responsive:{
+            1000:{
+                items: 6,
+                singleItem: true,
+                nav:true,
+                center: true
+            }
+        }
+    });       
 })

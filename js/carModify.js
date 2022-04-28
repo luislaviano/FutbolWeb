a79e84fb8062d1100db3e0bcbead2c45ae96d767
LuisL <luislaviano@gmail.com>
2022-04-28 01:52:33 -0300

    flags added in sidebar to qatar

---
+++ js/carModify.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // Activate Carousel
+    $('.carousel').attr(
+        "data-interval", "20000");
+
+})
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
- 
+
+
     $(".owl-carousel").owlCarousel({
   
       responsiveClass:true,
